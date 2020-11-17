acb11ada350cc6c18e06bcb9b2f23ac62381c413
erickcuevasc <47923706+erickcuevasc@users.noreply.github.com>
2020-11-17 12:59:02 -0600

    Delete 8.js

--- Series/8.js
+++
@@ -1,9 +0,0 @@
-//Imprime primeros 10 números con potencia X elevado a la Y
-//Erick Cuevas Ceballos 3°F
-var r;
-for(i = 1; i <= 10; i++){
-    var x = Math.round(Math.random()*(10-1)*1);
-    var y = Math.round(Math.random()*(10-1)*1); 
-    r  = Math.pow(x,y);
-      console.log('Potencia de: '+ x +' elevado a:' + y + ' igual a:' + r);
-  }
